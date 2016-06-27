e1b4e6d969f33522e59fe58a6b44f473c07ca728
Oleksiy Hryshchenko <greeschenko@gmail.com>
2016-06-28 00:43:03 +0300

    up

--- models/User.php
+++ models/User.php
@@ -3,6 +3,10 @@
 namespace greeschenko\user\models;
 
 use Yii;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+use yii\behaviors\TimestampBehavior;
+use yii\base\NotSupportedException;
 
 /**
  * This is the model class for table "{{%user}}".
@@ -19,8 +23,16 @@ use Yii;
  *
  * @property Profile $profile
  */
-class User extends \yii\db\ActiveRecord
+class User extends ActiveRecord implements IdentityInterface
 {
+    const STATUS_DELETED = 0;
+    const STATUS_ACTIVE = 10;
+
+    public $password;
+    public $oldpassword;
+    public $newpassword;
+    public $newpasswordre;
+
     /**
      * @inheritdoc
      */
@@ -32,10 +44,44 @@ class User extends \yii\db\ActiveRecord
     /**
      * @inheritdoc
      */
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::className(),
+        ];
+    }
+
+    public function beforeSave($insert)
+    {
+        $this->username = $this->email;
+
+        if (!$this->isNewRecord) {
+            if ($this->newpassword != '') {
+                $this->setPassword($this->newpassword);
+                $this->removePasswordResetToken();
+            }
+        }
+
+        if (
+            $this->oldpassword != ''
+            and $this->newpassword != ''
+            and $this->newpasswordre != ''
+            and $this->newpassword == $this->newpasswordre
+        ) {
+            $this->setPassword($this->newpassword);
+            $this->removePasswordResetToken();
+        }
+
+        return parent::beforeSave($insert);
+    }
+
+    /**
+     * @inheritdoc
+     */
     public function rules()
     {
         return [
-            [['username', 'auth_key', 'password_hash', 'email', 'created_at', 'updated_at'], 'required'],
+            [['username', 'auth_key', 'password_hash', 'email'], 'required'],
             [['status', 'created_at', 'updated_at'], 'integer'],
             [['username', 'password_hash', 'password_reset_token', 'email'], 'string', 'max' => 255],
             [['auth_key'], 'string', 'max' => 32],
@@ -43,6 +89,13 @@ class User extends \yii\db\ActiveRecord
             [['email'], 'unique'],
             [['password_reset_token'], 'unique'],
         ];
+    }
+
+    public function checkPassword($attribute, $params)
+    {
+        if ( !$this->validatePassword($this->$attribute) ) {
+            $this->addError($attribute, Yii::t('app', 'Wrong password'));
+        }
     }
 
     /**
@@ -61,6 +114,171 @@ class User extends \yii\db\ActiveRecord
             'created_at' => Yii::t('app', 'Created At'),
             'updated_at' => Yii::t('app', 'Updated At'),
         ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
+    }
+
+    /**
+     * Finds user by password reset token
+     *
+     * @param string $token password reset token
+     * @return static|null
+     */
+    public static function findByPasswordResetToken($token)
+    {
+        if (!static::isPasswordResetTokenValid($token)) {
+            return null;
+        }
+
+        return static::findOne([
+            'password_reset_token' => $token,
+            'status' => self::STATUS_ACTIVE,
+        ]);
+    }
+
+    /**
+     * Finds out if password reset token is valid
+     *
+     * @param string $token password reset token
+     * @return boolean
+     */
+    public static function isPasswordResetTokenValid($token)
+    {
+        if (empty($token)) {
+            return false;
+        }
+
+        $timestamp = (int) substr($token, strrpos($token, '_') + 1);
+        $expire = Yii::$app->params['user.passwordResetTokenExpire'];
+
+        return $timestamp + $expire >= time();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getId()
+    {
+        return $this->getPrimaryKey();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->getAuthKey() === $authKey;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return boolean if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return Yii::$app->security->validatePassword($password, $this->password_hash);
+    }
+
+    /**
+     * Generates password hash from password and sets it to the model
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+    }
+
+    /**
+     * Generates "remember me" authentication key
+     */
+    public function generateAuthKey()
+    {
+        $this->auth_key = Yii::$app->security->generateRandomString();
+    }
+
+    /**
+     * Generates new password reset token
+     */
+    public function generatePasswordResetToken()
+    {
+        $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
+    }
+
+    /**
+     * Removes password reset token
+     */
+    public function removePasswordResetToken()
+    {
+        $this->password_reset_token = null;
+    }
+
+    /**
+     * Signs user up.
+     *
+     * @return User|null the saved model or null if saving fails
+     */
+    public function signup()
+    {
+        if ($this->validate()) {
+            $this->setPassword($this->password);
+            $this->generateAuthKey();
+            if ($this->save()) {
+                return $this;
+            }
+        }
+
+        return null;
+    }
+
+    public function genRandomString()
+    {
+        $password=null;
+        $chars="qazxswedcvfrtgbnhyujmkiolp
+            1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP
+            .~,()[]{}";
+        $max=10;
+        $size=StrLen($chars)-1;
+        while ($max--) {
+            $password.=$chars[rand(0,$size)];
+        }
+
+        return $password;
     }
 
     /**
---
+++ tests/codeception/acceptance/LoginCept.php
@@ -0,0 +1,40 @@
+<?php
+
+use tests\codeception\_pages\LoginPage;
+
+/* @var $scenario Codeception\Scenario */
+
+$I = new AcceptanceTester($scenario);
+$I->wantTo('ensure that login works');
+
+$loginPage = LoginPage::openBy($I);
+
+$I->see('Login', 'h1');
+
+$I->amGoingTo('try to login with empty credentials');
+$loginPage->login('', '');
+if (method_exists($I, 'wait')) {
+    $I->wait(1);
+}
+$I->expectTo('see validations errors');
+$I->see('Email cannot be blank.');
+$I->see('Password cannot be blank.');
+
+$I->amGoingTo('try to login with wrong credentials');
+$loginPage->login('admin', 'wrong');
+if (method_exists($I, 'wait')) {
+    $I->wait(1);
+}
+$I->expectTo('see validations errors');
+$I->see('Incorrect username or password.');
+
+$I->amGoingTo('try to login with correct credentials');
+$loginPage->login('root', 'rootpass');
+if (method_exists($I, 'wait')) {
+    $I->wait(1);
+}
+$I->expectTo('see user info');
+$I->dontSee('Incorrect username or password.');
+if (method_exists($I, 'wait')) {
+    $I->wait(1);
+}
